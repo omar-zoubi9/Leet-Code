dc52ed093d321cf97d46a16cd45478d89f6502a4
omar-zoubi9 <omar10zoubi@gmail.com>
2022-08-01 00:07:13 +0300

    test

---
+++ src/QuestionsToAceInterviews/RunningSum.java
@@ -0,0 +1,26 @@
+package QuestionsToAceInterviews;
+
+import org.jetbrains.annotations.Contract;
+
+import java.util.HashMap;
+
+//https://leetcode.com/problems/running-sum-of-1d-array/
+public class RunningSum {
+    public static void main(String[] args) {
+        int[] nums=new int[3];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums=sol(nums);
+        for(int i=0 ;i < nums.length ; i++)
+            System.out.println(nums[i] + " ");
+    }
+
+    static int[] sol(int[] nums) {
+        for(int i=1 ; i < nums.length ; i++){
+            nums[i]+=nums[i-1];
+        }
+
+        return nums;
+    }
+}
